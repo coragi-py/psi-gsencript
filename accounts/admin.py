9ec3a3e1beb87e03b033bd1824db087f536bdb5d
coragi-py <fabioyuuki8@gmail.com>
2026-05-08 03:50:41 -0300

    Fix: Corrigido footer duplicado

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+
+    # Campos a serem exibidos na lista de usuários
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_joined')
+
+    #Ordenação
+    ordering = ('email',)
+
+    # Filtros laterais para facilitar a busca
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    # Barra de busca para encontrar usuários rapidamente
+    search_fields = ('email', 'first_name', 'last_name')
+
+    # Campos somente leitura para evitar alterações acidentais
+    readonly_fields = ('date_joined',)
+
+    # Configuração dos campos dentro da edição do usuário
+    # Campo de segredo 2FA apenas como leitura
+    fieldsets = UserAdmin.fieldsets + (
+        ('Autenticação de Dois Fatores', {
+            'fields': ('two_factor_secret', 'recovery_token', 'token_expiration'),
+        }),
+    )
+
+    readonly_fields = ('two_factor_secret', 'recovery_token', 'token_expiration')
